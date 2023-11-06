b8ea6a4e5fcc7b58d3195a0d65bc4ca0e5d98be8
rviprofile <rvi.profile@gmail.com>
2023-11-06 14:00:57 +0500

    activeBurger

--- src/components/navMenu.js
+++ src/components/navMenu.js
@@ -1,33 +1,23 @@
+import NavMenuList from "./navMenuList";
+import React from "react";
+const { useState } = React;
+
+
+
 export default function NavMenu() {
+  const [visible, setVisible] = useState(true);
+  const activeBurger = () => setVisible(!visible)
   return (
     <nav className="main__nav nav">
       <div className="nav__logo logo">
         <img className="logo__image" src="img/logo.png" alt="logo" />
       </div>
-      <div className="nav__burger burger">
+      <div className="nav__burger burger" onClick={activeBurger}>
         <span className="burger__line"></span>
         <span className="burger__line"></span>
         <span className="burger__line"></span>
       </div>
-      <div className="nav__menu menu">
-        <ul className="menu__list">
-          <li className="menu__item">
-            <a href="#" className="menu__link">
-              Главное
-            </a>
-          </li>
-          <li className="menu__item">
-            <a href="#" className="menu__link">
-              Мой плейлист
-            </a>
-          </li>
-          <li className="menu__item">
-            <a href="../signin.html" className="menu__link">
-              Войти
-            </a>
-          </li>
-        </ul>
-      </div>
+      {visible ? <NavMenuList /> : ''}
     </nav>
   );
 }
