8f79af82e51919dc1dae0af0b60bc5a181c65259
rviprofile <rvi.profile@gmail.com>
2023-11-28 15:39:07 +0500

    MainPage

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import React from "react";
 import * as S from "./styles.js"
-import { useEffect, useState } from "react";
-import AudioPlayer from "./components/audioPlayer/audioPlayer";
-import AudioPlayerSkeleton from "./components/skeletons/audioPlayerSkeleton";
-import NavMenu from "./components/navMenu/navMenu.js";
-import Sidebar from "./components/sidebar/sidebar.js";
-import SidebarSkeleton from "./components/skeletons/sidebarSkeleton";
-import TrackList from "./components/tracklist/trackList.js";
-import TrackListSkeleton from "./components/skeletons/trackListSkeleton";
-import loadingDelay from "./components/loading";
 import { AppRoutes } from "./routes.jsx";
 
 function App() {
-
-  // Псевдозагрузка
-  const [load, setLoad] = useState(true);
-  useEffect(() => {
-    loadingDelay(load, setLoad, 2000);
-  }, []);
-
   return (
     <>
     <S.GlobalStyle />
-    <S.Wrapper>
     <AppRoutes/>
-      <S.Container>
-        <S.Main>
-          <NavMenu />
-          {load ? <TrackListSkeleton /> : <TrackList />}
-          {load ? <SidebarSkeleton /> : <Sidebar />}
-        </S.Main>
-        {load ? <AudioPlayerSkeleton /> : <AudioPlayer />}
-        <footer className="footer"></footer>
-      </S.Container>
-    </S.Wrapper>
     </>
   );
 }
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,7 +1,33 @@
+import React from "react";
+import * as S from "../../styles.js";
+import { useEffect, useState } from "react";
+import AudioPlayer from "../../components/audioPlayer/audioPlayer.js";
+import AudioPlayerSkeleton from "../../components/skeletons/audioPlayerSkeleton";
+import NavMenu from "../../components/navMenu/navMenu.js";
+import Sidebar from "../../components/sidebar/sidebar.js";
+import SidebarSkeleton from "../../components/skeletons/sidebarSkeleton";
+import TrackList from "../../components/tracklist/trackList.js";
+import TrackListSkeleton from "../../components/skeletons/trackListSkeleton";
+import loadingDelay from "../../components/loading";
+
 export const MainPage = () => {
-    return (
-      <div>
-        <h1>Main page</h1>
-      </div>
-    );
-  };
+  // Псевдозагрузка
+  const [load, setLoad] = useState(true);
+  useEffect(() => {
+    loadingDelay(load, setLoad, 2000);
+  }, []);
+
+  return (
+    <S.Wrapper>
+      <S.Container>
+        <S.Main>
+          <NavMenu />
+          {load ? <TrackListSkeleton /> : <TrackList />}
+          {load ? <SidebarSkeleton /> : <Sidebar />}
+        </S.Main>
+        {load ? <AudioPlayerSkeleton /> : <AudioPlayer />}
+        <footer className="footer"></footer>
+      </S.Container>
+    </S.Wrapper>
+  );
+};
